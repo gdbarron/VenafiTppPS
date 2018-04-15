4513c70eb20524ed0e05976dc449c9171f1272dd
Greg Brownstein <greg@jagtechnical.com>
2018-04-15 11:37:53 -0400

    build fixes

--- Build/build.ps1
+++ Build/build.ps1
@@ -1,49 +1,16 @@
-function Resolve-Module {
-    [Cmdletbinding()]
-    param
-    (
-        [Parameter(Mandatory)]
-        [string[]]$Name
-    )
+param(
+    $Task = 'Default'
+)
 
-    Process {
-        foreach ($ModuleName in $Name) {
-            $Module = Get-Module -Name $ModuleName -ListAvailable
-            Write-Verbose -Message "Resolving Module $($ModuleName)"
-            
-            if ($Module) {                
-                $Version = $Module | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
-                $GalleryVersion = Find-Module -Name $ModuleName -Repository PSGallery | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
-
-                if ($Version -lt $GalleryVersion) {
-                    
-                    if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') {
-                        Set-PSRepository -Name PSGallery -InstallationPolicy Trusted 
-                    }
-                    
-                    Write-Verbose -Message "$($ModuleName) Installed Version [$($Version.tostring())] is outdated. Installing Gallery Version [$($GalleryVersion.tostring())]"
-                    
-                    Install-Module -Name $ModuleName -Force
-                    Import-Module -Name $ModuleName -Force -RequiredVersion $GalleryVersion
-                } else {
-                    Write-Verbose -Message "Module Installed, Importing $($ModuleName)"
-                    Import-Module -Name $ModuleName -Force -RequiredVersion $Version
-                }
-            } else {
-                Write-Verbose -Message "$($ModuleName) Missing, installing Module"
-                Install-Module -Name $ModuleName -Force
-                Import-Module -Name $ModuleName -Force -RequiredVersion $Version
-            }
-        }
-    }
-}
-
-# Grab nuget bits, install modules, set build variables, start build.
+# dependencies
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+if (-not (Get-Module -ListAvailable PSDepend)) {
+    & (Resolve-Path "$PSScriptRoot\helpers\Install-PSDepend.ps1")
+}
+Import-Module PSDepend
+$null = Invoke-PSDepend -Path "$PSScriptRoot\build.requirements.psd1" -Install -Import -Force
 
-Resolve-Module Psake, PSDeploy, Pester, BuildHelpers
+Set-BuildEnvironment -Force -Path $PSScriptRoot\..
 
-Set-BuildEnvironment
-
-Invoke-psake .\build\psake.ps1
+Invoke-psake $PSScriptRoot\psake.ps1 -taskList $Task -nologo
 exit ( [int]( -not $psake.build_success ) )
