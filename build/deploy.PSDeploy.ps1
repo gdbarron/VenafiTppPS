048103318e69d7c19ee80320dc71b45685dd6fef
Greg Brownstein <greg@jagtechnical.com>
2018-04-19 00:24:23 -0400

    build testing and !deploy

--- build/deploy.PSDeploy.ps1
+++ build/deploy.PSDeploy.ps1
@@ -18,16 +18,27 @@
 # * Set-BuildEnvironment from BuildHelpers module has populated ENV:BHModulePath and related variables
 
 # Publish to gallery with a few restrictions
-if ($ENV:BHProjectName -and $ENV:BHProjectName.Count -eq 1) {
+if (
+    $env:BHModulePath -and
+    $env:BHBuildSystem -ne 'Unknown' -and
+    $env:BHBranchName -eq "master" -and
+    $env:BHCommitMessage -match '!deploy'
+) {
     Deploy Module {
         By PSGalleryModule {
-            FromSource $ENV:BHProjectName
+            FromSource $ENV:BHModulePath
             To PSGallery
             WithOptions @{
                 ApiKey = $ENV:NugetApiKey
             }
         }
     }
+} else {
+    "Skipping deployment: To deploy, ensure that...`n" +
+    "`t* You are in a known build system (Current: $ENV:BHBuildSystem)`n" +
+    "`t* You are committing to the master branch (Current: $ENV:BHBranchName) `n" +
+    "`t* Your commit message includes !deploy (Current: $ENV:BHCommitMessage)" |
+        Write-Host
 }
 
 # Publish to AppVeyor if we're in AppVeyor
