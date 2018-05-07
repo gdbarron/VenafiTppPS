23275a7fdbb16600ef0c5cc2e3c041c4a43413bb
Greg Brownstein <greg@jagtechnical.com>
2018-05-07 15:39:36 -0400

    License and build updates and !deploy (#29)
    * fix nupkg and zip getting pushed to master during deploy, add license
    
    * publish to appveyor
    
    * appveyor deploy updates
    
    * update version of psdeploy and buildhelpers
    
    * more build testing
    
    * more build updates and !verbose
    
    * get module versions
    
    * build updates
    
    * build updates
    
    * disable appveyor due to undiagnosed failure

--- build/build.ps1
+++ build/build.ps1
@@ -5,9 +5,9 @@ Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
 Install-Module Psake, PSDeploy, BuildHelpers, platyPS, PSScriptAnalyzer -force
 Install-Module Pester -Force -SkipPublisherCheck
-Import-Module Psake, BuildHelpers, platyPS, PSScriptAnalyzer
+Import-Module Psake, BuildHelpers, platyPS, PSScriptAnalyzer, PSDeploy -force
 
 Set-BuildEnvironment
 
-Invoke-psake -buildFile .\build\psake.ps1 -taskList $Task -nologo
+Invoke-psake -buildFile .\build\psake.ps1 -taskList $Task -nologo -verbose
 exit ([int](-not $psake.build_success))
