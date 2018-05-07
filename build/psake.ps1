d6eaef6a633377eae864731b1e25f8fbd349570f
Greg Brownstein <greg@jagtechnical.com>
2018-05-07 11:04:17 -0400

    build updates

--- build/psake.ps1
+++ build/psake.ps1
@@ -340,18 +340,18 @@ Task Deploy -Depends BuildDocs {
     }
 
     # Publish to AppVeyor if we're in AppVeyor
-    # if (
-    #     $env:BHProjectName -and $ENV:BHProjectName.Count -eq 1 -and
-    #     $env:BHBuildSystem -eq 'AppVeyor'
-    # ) {
-    #     Deploy DeveloperBuild {
-    #         By AppVeyorModule {
-    #             FromSource $ENV:BHProjectName
-    #             To AppVeyor
-    #             WithOptions @{
-    #                 Version = $env:APPVEYOR_BUILD_VERSION
-    #             }
-    #         }
-    #     }
-    # }
+    if (
+        $env:BHProjectName -and $ENV:BHProjectName.Count -eq 1 -and
+        $env:BHBuildSystem -eq 'AppVeyor'
+    ) {
+        Deploy DeveloperBuild {
+            By AppVeyorModule {
+                FromSource $ENV:BHProjectName
+                To AppVeyor
+                WithOptions @{
+                    Version = $env:APPVEYOR_BUILD_VERSION
+                }
+            }
+        }
+    }
 }
