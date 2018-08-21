e5e9f38db8e75cd5417f4d3e00e8917e78be09ea
Greg Brownstein <greg@jagtechnical.com>
2018-08-20 20:24:37 -0400

    finish support for local provider type, update help

--- VenafiTppPS/Code/Public/Get-TppPermission.ps1
+++ VenafiTppPS/Code/Public/Get-TppPermission.ps1
@@ -1,13 +1,21 @@
 ﻿<#
 .SYNOPSIS
-Get basic or detailed certificate information
+Get permissions for TPP objects
 
 .DESCRIPTION
-Get certificate info based on a variety of attributes.
-Additional details can be had by passing the guid.
+Determine who has rights for TPP objects and what those rights are
 
 .PARAMETER Guid
-Guid representing a unique certificate in Venafi.
+Guid representing a unique object in Venafi.
+
+.PARAMETER ExternalProviderType
+External provider type with users/groups to assign permissions.  AD and LDAP are currently supported.
+
+.PARAMETER ExternalProviderName
+Name of the external provider as configured in TPP
+
+.PARAMETER UniversalId
+The id that represents the user or group.  Use Get-TppIdentity to get the id.
 
 .PARAMETER TppSession
 Session object created from New-TppSession method.  The value defaults to the script session object $TppSession.
@@ -16,52 +24,44 @@ Session object created from New-TppSession method.  The value defaults to the sc
 Guid
 
 .OUTPUTS
-ByPath and NoPath parameter sets returns a PSCustomObject with the following properties:
-    CreatedOn
-    DN
-    Guid
-    Name
-    ParentDn
-    SchemaClass
-    _links
+List parameter set returns a PSCustomObject with the properties ObjectGuid and Permissions
 
-Guid returns a PSCustomObject with the following properties:
-    CertificateAuthorityDN
-    CertificateDetails
-    Consumers
-    Contact
-    CreatedOn
-    CustomFields
-    Description
-    DN
-    Guid
-    ManagementType
-    Name
-    ParentDn
-    ProcessingDetails
-    RenewalDetails
-    SchemaClass
-    ValidationDetails
+Local and external parameter sets returns a PSCustomObject with the following properties:
+    ObjectGuid
+    ProviderType
+    ProviderName
+    UniversalId
+    EffectivePermissions (if Effective switch is used)
+    ExplicitPermissions (if Effective switch is NOT used)
+    ImplicitPermissions (if Effective switch is NOT used)
 
 .EXAMPLE
-Get-TppCertificateDetail -ExpireBefore ([DateTime] "2018-01-01")
-Find all certificates expiring before a certain date
+Get-TppObject -Path '\VED\Policy\My folder' | Get-TppPermission
+ObjectGuid                             Permissions
+----                                   -----------
+{1234abcd-g6g6-h7h7-faaf-f50cd6610cba} {AD+mydomain.com:1234567890olikujyhtgrfedwsqa, AD+mydomain.com:azsxdcfvgbhnjmlk09877654321}
+
+Get permissions for a specific policy folder
 
 .EXAMPLE
-Get-TppCertificateDetail -ExpireBefore ([DateTime] "2018-01-01") -Limit 5
-Find 5 certificates expiring before a certain date
+Get-TppObject -Path '\VED\Policy\My folder' | Get-TppPermission -Effective
+ObjectGuid           : {1234abcd-g6g6-h7h7-faaf-f50cd6610cba}
+ProviderType         : AD
+ProviderName         : mydomain.com
+UniversalId          : 1234567890olikujyhtgrfedwsqa
+EffectivePermissions : @{IsAssociateAllowed=False; IsCreateAllowed=True; IsDeleteAllowed=True; IsManagePermissionsAllowed=True; IsPolicyWriteAllowed=True;
+                       IsPrivateKeyReadAllowed=True; IsPrivateKeyWriteAllowed=True; IsReadAllowed=True; IsRenameAllowed=True; IsRevokeAllowed=False; IsViewAllowed=True;
+                       IsWriteAllowed=True}
 
-.EXAMPLE
-Get-TppCertificateDetail -Path '\VED\Policy\My Policy'
-Find all certificates in a specific path
+ObjectGuid           : {1234abcd-g6g6-h7h7-faaf-f50cd6610cba}
+ProviderType         : AD
+ProviderName         : mydomain.com
+UniversalId          : azsxdcfvgbhnjmlk09877654321
+EffectivePermissions : @{IsAssociateAllowed=False; IsCreateAllowed=False; IsDeleteAllowed=False; IsManagePermissionsAllowed=False; IsPolicyWriteAllowed=True;
+                       IsPrivateKeyReadAllowed=False; IsPrivateKeyWriteAllowed=False; IsReadAllowed=True; IsRenameAllowed=False; IsRevokeAllowed=True; IsViewAllowed=False;
+                       IsWriteAllowed=True}
 
-.EXAMPLE
-Get-TppCertificateDetail -Path '\VED\Policy\My Policy' -Recursive
-Find all certificates in a specific path and all subfolders
-
-.EXAMPLE
-Get-TppCertificateDetail -ExpireBefore ([DateTime] "2018-01-01") -Limit 5 | Get-TppCertificateDetail
-Get detailed certificate info on the first 5 certificates expiring before a certain date
+Get effective permissions for a specific policy folder
 
 .LINK
 http://venafitppps.readthedocs.io/en/latest/functions/Get-TppPermission/
@@ -137,32 +137,47 @@ function Get-TppPermission {
                         $perms.ForEach{
                             # get details from list of perms on the object
                             # loop through and get effective perms on each by re-calling this function
-                            # TODO: update split to support local
-                            $type, $name, $id = $_.Split('+:')
-                            $effectiveParams = @{
-                                Guid                 = $thisGuid
-                                ExternalProviderType = $type
-                                ExternalProviderName = $name
-                                UniversalId          = $id
-                                Effective            = $true
+
+                            if ( $_.StartsWith('local:') ) {
+
+                                $type, $id = $_.Split(':')
+                                $effectiveParams = @{
+                                    Guid        = $thisGuid
+                                    UniversalId = $id
+                                    Effective   = $true
+                                }
+                            } else {
+                                $type, $name, $id = $_.Split('+:')
+                                $effectiveParams = @{
+                                    Guid                 = $thisGuid
+                                    ExternalProviderType = $type
+                                    ExternalProviderName = $name
+                                    UniversalId          = $id
+                                    Effective            = $true
+                                }
                             }
                             Get-TppPermission @effectiveParams
                         }
                     } else {
                         # just list out users/groups with rights
                         [PSCustomObject] @{
-                            GUID        = $thisGuid
+                            ObjectGuid  = $thisGuid
                             Permissions = $perms
                         }
                     }
                 }
 
                 {$_ -in 'Local', 'External'} {
+
                     # different URLs if local vs external
                     if ( $PSBoundParameters.ContainsKey('ExternalProviderType') ) {
                         $params.UriLeaf += "/$ExternalProviderType/$ExternalProviderName/$UniversalId"
+                        $providerType = $ExternalProviderType
+                        $providerName = $ExternalProviderName
                     } else {
                         $params.UriLeaf += "/local/$UniversalId"
+                        $providerType = 'local'
+                        $providerName = ''
                     }
 
                     if ( $PSBoundParameters.ContainsKey('Effective') ) {
@@ -171,18 +186,25 @@ function Get-TppPermission {
 
                     $response = Invoke-TppRestMethod @params
 
+                    $returnObject = [PSCustomObject] @{
+                        ObjectGuid   = $thisGuid
+                        ProviderType = $providerType
+                        ProviderName = $providerName
+                        UniversalId  = $UniversalId
+                    }
+
                     if ( $PSBoundParameters.ContainsKey('Effective') ) {
-                        [PSCustomObject] @{
-                            GUID                 = $thisGuid
+                        $returnObject | Add-Member @{
                             EffectivePermissions = $response.EffectivePermissions
                         }
                     } else {
-                        [PSCustomObject] @{
-                            GUID                = $thisGuid
+                        $returnObject | Add-Member @{
                             ExplicitPermissions = $response.ExplicitPermissions
                             ImplicitPermissions = $response.ImplicitPermissions
                         }
                     }
+
+                    $returnObject
                 }
             }
         }
