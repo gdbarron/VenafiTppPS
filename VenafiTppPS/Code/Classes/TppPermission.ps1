795a3341c6c57dab9c10a5af508337a087f513df
Greg Brownstein <greg@jagtechnical.com>
2018-08-30 10:25:07 -0400

    add TppPermission class

---
+++ VenafiTppPS/Code/Classes/TppPermission.ps1
@@ -0,0 +1,17 @@
+class TppPermission {
+
+    [bool] $IsAssociateAllowed
+    [bool] $IsCreateAllowed
+    [bool] $IsDeleteAllowed
+    [bool] $IsManagePermissionsAllowed
+    [bool] $IsPolicyWriteAllowed
+    [bool] $IsPrivateKeyReadAllowed
+    [bool] $IsPrivateKeyWriteAllowed
+    [bool] $IsReadAllowed
+    [bool] $IsRenameAllowed
+    [bool] $IsRevokeAllowed
+    [bool] $IsViewAllowed
+    [bool] $IsWriteAllowed
+
+}
+
