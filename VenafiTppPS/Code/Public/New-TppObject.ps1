fff3f8004a7e8a0b161df4e124309bf91ba0d50a
Brownstein <gbrownstein@deloitte.com>
2019-10-02 10:43:39 -0400

    set attribute param to non mandatory

--- VenafiTppPS/Code/Public/New-TppObject.ps1
+++ VenafiTppPS/Code/Public/New-TppObject.ps1
@@ -81,7 +81,7 @@ function New-TppObject {
         [Parameter(Mandatory)]
         [String] $Class,
 
-        [Parameter(Mandatory)]
+        [Parameter()]
         [ValidateNotNullOrEmpty()]
         [Hashtable] $Attribute,
 
@@ -121,7 +121,7 @@ function New-TppObject {
         }
     }
 
-    if ( $Attribute ) {
+    if ( $PSBoundParameters.ContainsKey('Attribute') ) {
         # api requires a list of hashtables for nameattributelist
         # with 2 items per hashtable, with key names 'name' and 'value'
         # this is cumbersome for the user so allow them to pass a standard hashtable and convert it for them
